65d6843274687b2facf6b8839e037d0976e97c5c
Simon Hager <151155264+s4hager@users.noreply.github.com>
2024-01-05 18:53:34 +0100

    Update index.php

--- data/html/index.php
+++ data/html/index.php
@@ -4,14 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FAQ: Docker & Kubernetes</title>
+    <title>FAQ: Docker & Docker Swarm</title>
     <link rel="stylesheet" href="style.css">
 
-    <link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!--<link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
-	<div class="text-center"><h1>FAQ Docker & Kubernetes</h1></div>
+	<div class="text-center"><h1>FAQ von Docker & Docker Swarm</h1></div>
 
 <?php
 $conn = new mysqli('mariadb', 'student', 'student123');
